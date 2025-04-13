835fe06d2943eaeb8c2cf5553e27d08522aa4de8
kushagra <agrakush4@gmail.com>
2025-04-14 02:47:15 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,60 @@
+// scrolling facts 
+let scrollingElement = document.querySelector('#left');
+let n = 1.1;
+  setInterval(function()  {
+      let x =  scrollingElement.scrollTop;
+      scrollingElement.scrollTop += n;
+      if(x>=scrollingElement.scrollHeight-scrollingElement.clientHeight){
+        scrollingElement.scrollTop=0;
+    }
+    },20);
+scrollingElement.addEventListener("mouseleave",function(){
+    n=1.1;
+})
+scrollingElement.addEventListener("mouseenter",function(){
+    n=0;
+})
+
+
+// selection on divclick
+function selectOption(radioId) {
+    document.getElementById(radioId).checked = true;
+}
+
+//quiz using api
+let options;
+let questiondata;
+async function fq() {
+    document.querySelector('#right').style.backgroundColor = "";
+    document.querySelector('#newq').innerHTML='Next question'
+    let response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
+    let data = await response.json()
+    questiondata= data.results[0];
+    console.log(questiondata)
+    // display question
+    document.querySelector('#quesh').innerHTML=questiondata.question
+
+    // displaying options
+     options = [...questiondata.incorrect_answers, questiondata.correct_answer];
+    for (let i = 0; i < options.length; i++) {
+        document.getElementById(`opt${i+1}`).value = options[i];
+        document.getElementById(`label${i+1}`).innerHTML = options[i];
+    }
+
+}
+
+// event listener for fq
+let selectedOption;
+document.querySelector('#newq').addEventListener('click',fq)
+document.querySelector('#subbtn').addEventListener('click',function () {
+     selectedOption = document.querySelector('input[name="opts"]:checked');
+    if (!selectedOption) {
+        alert("Please select an option ");
+    }
+    if (selectedOption.value === questiondata.correct_answer) {
+        document.querySelector('#right').style.backgroundColor = "#90ee90";
+    }
+    else{
+        document.querySelector('#right').style.backgroundColor = "#ff6961";
+    }
+})
